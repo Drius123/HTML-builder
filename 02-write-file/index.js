19a8336cc4ba1570ffe5993beb81aff650b4656d
Андрей <adovgulev111@gmail.com>
2023-05-06 14:05:24 +0300

    feat: completed task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const path = require('node:path');
+const fs = require('fs');
+const process = require('node:process');
+const pathToFile = path.join(__dirname, 'text.txt')
+
+const pToFile = fs.createWriteStream(pathToFile);
+
+process.stdout.write('Приветствие! Введите текст \n');
+process.stdin.on('data', (data) => {
+	if (data.toString().trim() === 'exit') {
+		process.stdout.write('Выполнен выход из процесса записи текста');
+		pToFile.end();
+		process.exit();
+	} else {
+		pToFile.write(data);
+	}
+})
+process.on('SIGINT', () => {
+  process.stdout.write('Ввод закончен <Ctrl + C>. Проверьте файл text.txt. Удачи'); 
+  process.exit();
+}); 
