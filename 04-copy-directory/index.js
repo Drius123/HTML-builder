14689cc99f60d748f32d46979aaa0a04c752e9ee
Андрей <adovgulev111@gmail.com>
2023-05-06 17:22:27 +0300

    feat: completed task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const path = require('node:path');
+const fs = require('fs');
+
+const pathToDir = path.join(__dirname, 'files');
+const pathToDirNew = path.join(__dirname, 'files-copy');
+
+fs.mkdir(pathToDirNew, {recursive: true}, (err) => {
+	if(err) {
+		console.log(err);
+	}
+});
+fs.readdir(pathToDir, {withFileTypes: true}, (err, files) => {
+	if(err) {
+		console.log(err)
+	} else {
+		files.forEach(item => {
+			if(item.isFile()) {
+				fs.copyFile(path.join(__dirname, 'files', item.name), path.join(__dirname, 'files-copy', item.name), (err) => {
+					if(err) {
+						console.log(err);
+					}
+				});
+			}})
+	}
+});
 
